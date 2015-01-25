5b5f9de7b4f48d725b541876e2e1fc583c9fd52c
ninghanglin365 <ninghang.lin@gmail.com>
2015-01-25 11:53:46 -0500

    Week3AM2
    First edition of code for assignment 2, week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the reverse
+## 4. get the value of the reverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        rev <- NULL
+        set <- function(y) {
+                x <<- y
+                rev <<- NULL
+        }
+        get <- function() x
+        setrev <- function(solve) rev <<- solve
+        getrev <- function() rev
+        list (set = set, get = get, setrev = setrev, getrev = getrev)
 
 }
 
 
-## Write a short comment describing this function
+## 1. test the existance of rev
+## 2. get the rev, and skips the computation
+## 3. calculate the rev and set the rev in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        rev <- x$getrev()
+        if(!is.null(rev)){
+                message("getting cached data")
+                return(rev)
+        }
+        matrix <- x$get()
+        rev <- solve(matrix, ...)
+        x$setrev(rev)
+        rev
+        
 }
+
+
+amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+amatrix$get()         # Returns original matrix
+cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
+amatrix$getrev()  # Returns matrix inverse
+cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
+amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
+cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
+amatrix$get()         # Returns matrix
+amatrix$getrev()  # Returns matrix inverse
