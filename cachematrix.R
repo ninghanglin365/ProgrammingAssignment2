c42b2a78253bcc5deeacdb91348476f104c1fd23
ninghanglin365 <ninghang.lin@gmail.com>
2015-01-25 12:00:26 -0500

    Change rev to inv
    use “inv” to better indicate inverse formate

--- cachematrix.R
+++ cachematrix.R
@@ -6,43 +6,33 @@
 ## 4. get the value of the reverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        rev <- NULL
+        inv <- NULL
         set <- function(y) {
                 x <<- y
-                rev <<- NULL
+                inv <<- NULL
         }
         get <- function() x
-        setrev <- function(solve) rev <<- solve
-        getrev <- function() rev
-        list (set = set, get = get, setrev = setrev, getrev = getrev)
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list (set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
 
-## 1. test the existance of rev
-## 2. get the rev, and skips the computation
-## 3. calculate the rev and set the rev in the cache
+## 1. test the existance of inv
+## 2. get the inv, and skips the computation
+## 3. calculate the inv and set the inv in the cache
 
 cacheSolve <- function(x, ...) {
-        rev <- x$getrev()
-        if(!is.null(rev)){
+        inv <- x$getinv()
+        if(!is.null(inv)){
                 message("getting cached data")
-                return(rev)
+                return(inv)
         }
         matrix <- x$get()
-        rev <- solve(matrix, ...)
-        x$setrev(rev)
-        rev
+        inv <- solve(matrix, ...)
+        x$setinv(inv)
+        inv
         
 }
 
-
-amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
-amatrix$get()         # Returns original matrix
-cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
-amatrix$getrev()  # Returns matrix inverse
-cacheSolve(amatrix)   # Returns cached matrix inverse using previously computed matrix inverse
-amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
-cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
-amatrix$get()         # Returns matrix
-amatrix$getrev()  # Returns matrix inverse
